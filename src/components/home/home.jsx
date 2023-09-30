7b12496db7dba9a11e4a48fc4e801fefd9db043a
Agustin Panizza <agustin_panizza@hotmail.com>
2023-09-30 17:52:09 -0300

    New design

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -13,7 +13,21 @@ function Home(props) {
   const planet5Ref = useRef(null);
   const planet6Ref = useRef(null);
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
   const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,19 +36,30 @@ function Home(props) {
       const maxScroll =
         document.documentElement.scrollHeight - window.innerHeight;
 
+      let speedScale = 0.035; // Valor predeterminado
+      if (windowWidth < 100) {
+        // Si el ancho de la pantalla es menor a 100, ajusta la velocidad
+        speedScale = 1.959;
+      }
       // Calcula la opacidad en función del scroll (0 cuando alcanza el 100% de scroll)
       const calculatedOpacity = 1 - Math.min(scrollY / maxScroll, 1);
 
-      asteroidContainer.style.transform = `scale(${1 + scrollY * 0.035})`;
+      asteroidContainer.style.transform = `scale(${1 + scrollY * speedScale})`;
+
+      let speedMultiplier = 1.5; // Valor predeterminado
+      if (windowWidth < 100) {
+        // Si el ancho de la pantalla es menor a 100, ajusta la velocidad
+        speedMultiplier = 3.5;
+      }
 
       // Calcula la posición horizontal en función del scroll (para los planetas 1, 2 y 3)
-      const leftPosition123 = -scrollY * 1.5; // Ajusta el valor según la velocidad de desplazamiento
+      const leftPosition123 = -scrollY * speedMultiplier; // Ajusta el valor según la velocidad de desplazamiento
       planet1Ref.current.style.left = `${leftPosition123}px`;
       planet2Ref.current.style.left = `${leftPosition123}px`;
       planet3Ref.current.style.left = `${leftPosition123}px`;
 
       // Calcula la posición horizontal en función del scroll (para los planetas 4, 5 y 6)
-      const rightPosition456 = scrollY * 1.5; // Ajusta el valor según la velocidad de desplazamiento
+      const rightPosition456 = scrollY * speedMultiplier; // Ajusta el valor según la velocidad de desplazamiento
       planet4Ref.current.style.left = `${rightPosition456}px`;
       planet5Ref.current.style.left = `${rightPosition456}px`;
       planet6Ref.current.style.left = `${rightPosition456}px`;
@@ -47,7 +72,7 @@ function Home(props) {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [windowWidth]);
 
   return (
     <ThemeProvider
@@ -84,30 +109,34 @@ function Home(props) {
           className={`${style.first} ${style.planeta2}`}
           ref={planet2Ref}
         />
-        <img
-          src="./img/planeta3.png"
-          alt="planeta3"
-          className={`${style.first} ${style.planeta3}`}
-          ref={planet3Ref}
-        />
-        <img
-          src="./img/planeta4.png"
-          alt="planeta4"
-          className={`${style.first} ${style.planeta4}`}
-          ref={planet4Ref}
-        />
-        <img
-          src="./img/planeta5.png"
-          alt="planeta5"
-          className={`${style.first} ${style.planeta5}`}
-          ref={planet5Ref}
-        />
-        <img
-          src="./img/planeta6.png"
-          alt="planeta6"
-          className={`${style.first} ${style.planeta6}`}
-          ref={planet6Ref}
-        />
+        {windowWidth > 1000 && (
+          <>
+            <img
+              src="./img/planeta3.png"
+              alt="planeta3"
+              className={`${style.first} ${style.planeta3}`}
+              ref={planet3Ref}
+            />
+            <img
+              src="./img/planeta4.png"
+              alt="planeta4"
+              className={`${style.first} ${style.planeta4}`}
+              ref={planet4Ref}
+            />
+            <img
+              src="./img/planeta5.png"
+              alt="planeta5"
+              className={`${style.first} ${style.planeta5}`}
+              ref={planet5Ref}
+            />
+            <img
+              src="./img/planeta6.png"
+              alt="planeta6"
+              className={`${style.first} ${style.planeta6}`}
+              ref={planet6Ref}
+            />
+          </>
+        )}
         <div
           className={style.asteroidContainer}
           ref={asteroidContainerRef}
